61bcd8c099015ce58db044f7d06560708b754113
Nathan <diicky@gmail.com>
2018-04-04 19:47:53 -0700

    Menu functionality

--- Menu/main.cpp
+++ Menu/main.cpp
@@ -5,48 +5,37 @@
 
 using namespace std;
 
-void ArithInt(Integer a, Integer b);
-void ArithDouble(Double c, Double d);
+Menu m;
+
+void func1();
+void func2();
+void func3();
+void Exit();
+
 int main()
 {
-    Integer a, b(3);
-    Integer e(2);
-    a.equals(3);
-    b.equals(-4);
-    Double c, d;
 
-    //Integer to Double
-
-
-    c.equals(3.5);
-    d.equals(6.5);
-    Double f(a), num(3.4), test(c);
-    cout << test.toDouble() << endl;
-    cout << num.toDouble() << endl;
-    cout << f.toDouble() << endl;
-    ArithInt(a, b);
-
-    ArithInt(b, a);
-    ArithDouble(c, d);
-    ArithDouble(d, c);
     return 0;
 }
-void ArithInt(Integer a, Integer b)
+
+void func1()
 {
-    cout << "The int: " << a.toInt() << " " << endl;
-    cout << "Add: " << a.add(b) << endl;
-    cout << "Sub: " << a.sub(b) << endl;
-    cout << "Mul: " << a.mul(b) << endl;
-    cout << "Div: " << a.div(b) << endl;
-    cout << endl;
+    cout << "Hello from function 1" << endl;
+    m.waitKey();
 }
-void ArithDouble(Double c, Double d)
+void func2()
 {
-    cout << "The double: " << c.toDouble() << " " << endl;
-    cout << "Add: " << c.add(d) << endl;
-    cout << "Sub: " << c.sub(d) << endl;
-    cout << "Mul: " << c.mul(d) << endl;
-    cout << "Div: " << c.div(d) << endl;
-    cout << endl;
+    cout << "Hello from function 2" << endl;
+    m.waitKey();
+}
+void func3()
+{
+    cout << "Hello from function 3" << endl;
+    m.waitKey();
+}
+void Exit()
+{
+    cout << "Goodbye" << endl;
+    exit(0);
 }
 
