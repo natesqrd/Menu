4c7f74d622c0de873b9e2346d0f514108f1305ce
Nathan <diicky@gmail.com>
2018-04-04 18:10:01 -0700

    Initial Commit

---
+++ Menu/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Menu
+//
+//  Created by Nathaniel Tucker on 4/4/18.
+//  Copyright © 2018 Nathaniel Tucker. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
